08e892b9ea46f6cc7d04076863f029452304bc36
abdalrahman <cgrovestreet@gmail.com>
2025-04-04 06:26:41 +0200

    come back

--- js/main.js
+++ js/main.js
@@ -2,14 +2,6 @@ let skills = document.getElementById('skills');
 let works = document.getElementById('works');
 let Contact = document.getElementById('Contact');
 let scrollUp = document.getElementById('scroll-up');
-let logoANE = document.querySelector('.logo');
-let nav1 = document.getElementById('scroll-1');
-let nav2 = document.getElementById('scroll-2');
-let nav3 = document.getElementById('scroll-3');
-let nav4 = document.getElementById('scroll-4');
-let nav5 = document.getElementById('scroll-5');
-let container = document.querySelector('.container');
-let container2 = document.querySelector('.right-sec');
 
 skills.onclick = function(){
     scroll({
@@ -41,23 +33,4 @@ scrollUp.onclick = function(){
         top:0,
         behavior:"smooth",
     })
-}
-window.onload = function(){
-    nav1.style.top = '0';
-    nav1.style.opacity = '1';
-    nav2.style.top = '0';
-    nav2.style.opacity = '1';
-    nav3.style.top = '0';
-    nav3.style.opacity = '1';
-    nav4.style.top = '0';
-    nav4.style.opacity = '1';
-    nav5.style.top = '0';
-    nav5.style.opacity = '1';
-    logoANE.style.left = '0';
-    logoANE.style.opacity = '1';
-    container.style.margin = '120px 150px 0 200px';
-    container.style.opacity = '1';
-    container2.style.margin = ' 0 0 0 260px'
-    container2.style.opacity = '1';
-    container2.style.right = '110px';
 }
